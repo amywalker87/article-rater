bc4124c6140295d5bb9baa3e7498f820f65ce683
Amy Walker <amy@Amys-iMac.local>
2018-09-15 15:02:32 +0100

    proper html tags

--- json-load.js
+++ json-load.js
@@ -8,7 +8,30 @@ function loadArticle(id){
 		article = JSON.parse(article.message);
         $("h1").append(article.title);
         for (var element in article.body){
-        	$("div.article-body").append('<div class="'+article.body[element].type+'">stuff</div>');
+        	if(article.body[element].type == "heading"){
+        		$("div.article-body").append('<h2>'+article.body[element].model.text+'</h2>');
+        	}
+        	else if(article.body[element].type == "paragraph"){
+        		$("div.article-body").append('<p>'+article.body[element].model.text+'</p>');
+        	}
+        	else if(article.body[element].type == "image"){
+        		$("div.article-body").append('<img src="'+article.body[element].model.url+'" height="'+article.body[element].model.height+'" width="'+article.body[element].model.width+'" alt="'+article.body[element].model.altText+'"/>');
+        	}
+        	else if(article.body[element].type == "list"){
+        		var listTag = "ol>";
+        		if(article.body[element].model.type == "unordered"){
+        			listTag = "ul>";
+        		}
+    			var listHtml = "<"+listTag;
+    			for (var list in article.body[element].model.items){
+    				listHtml += "<li>";
+    				listHtml += article.body[element].model.items[list];
+    				listHtml += "</li>";
+    			}
+    			listHtml += "</";
+    			listHtml += listTag;
+    			$("div.article-body").append(listHtml);
+        	}
         }
     });
 }
