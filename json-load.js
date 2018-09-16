25284e8840e7bdc5a9ad19c70c72e6eb98744e42
Amy Walker <amy@Amys-iMac.local>
2018-09-16 14:02:15 +0100

    nicer element creation

--- json-load.js
+++ json-load.js
@@ -4,33 +4,50 @@ var randomIndex = Math.ceil(Math.random() * articleArray.length);
 $(document).ready(loadArticle(randomIndex));
 
 function loadArticle(id){
-	$.getJSON("api/article/get.php?id="+id, function(article){
+	$.getJSON('api/article/get.php?id='+id, function(article){
 		article = JSON.parse(article.message);
-        $("h1").append(article.title);
+		var articleElement = document.querySelector('.article');
+
+		var h1 = document.createElement('h1');
+		h1.textContent = article.title;
+		h1.className = 'bg-primary text-white';
+		articleElement.appendChild(h1);
+
+		var bodyDiv = document.createElement('div');
+		bodyDiv.className = 'article-body';
+		articleElement.appendChild(bodyDiv);
+
         for (var element in article.body){
-        	if(article.body[element].type == "heading"){
-        		$("div.article-body").append('<h2>'+article.body[element].model.text+'</h2>');
+        	if(article.body[element].type == 'heading'){
+        		var h2 = document.createElement('h2');
+        		h2.textContent = article.body[element].model.text;
+        		bodyDiv.appendChild(h2);
         	}
-        	else if(article.body[element].type == "paragraph"){
-        		$("div.article-body").append('<p>'+article.body[element].model.text+'</p>');
+        	else if(article.body[element].type == 'paragraph'){
+        		var p = document.createElement('p');
+        		p.textContent = article.body[element].model.text;
+        		bodyDiv.appendChild(p);
         	}
-        	else if(article.body[element].type == "image"){
-        		$("div.article-body").append('<img class="img-fluid mx-auto d-block shadow-sm p-2 mb-3" src="'+article.body[element].model.url+'" max-height="'+article.body[element].model.height+'" max-width="'+article.body[element].model.width+'" alt="'+article.body[element].model.altText+'"/>');
+        	else if(article.body[element].type == 'image'){
+        		var img = document.createElement('img');
+        		img.className = 'img-fluid mx-auto d-block shadow-sm p-2 mb-3';
+        		img.src = article.body[element].model.url;
+        		img.alt = article.body[element].model.altText;
+        		img.height = article.body[element].model.height;
+        		img.width = article.body[element].model.width;
+        		bodyDiv.appendChild(img);
         	}
-        	else if(article.body[element].type == "list"){
-        		var listTag = "ol>";
-        		if(article.body[element].model.type == "unordered"){
-        			listTag = "ul>";
+        	else if(article.body[element].type == 'list'){
+        		var li = document.createElement('ol');
+        		if(article.body[element].model.type == 'unordered'){
+        			li = document.createElement('ul');
         		}
-    			var listHtml = "<"+listTag;
     			for (var list in article.body[element].model.items){
-    				listHtml += "<li>";
-    				listHtml += article.body[element].model.items[list];
-    				listHtml += "</li>";
+    				var li2 = document.createElement('li');
+    				li2.textContent = article.body[element].model.items[list];
+    				li.appendChild(li2);
     			}
-    			listHtml += "</";
-    			listHtml += listTag;
-    			$("div.article-body").append(listHtml);
+    			bodyDiv.appendChild(li);
         	}
         }
     });
