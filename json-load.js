77ca3f41cf4fd758719b724e3bbf17b701bb2d2c
Amy Walker <amy@Amys-iMac.local>
2018-09-16 21:23:53 +0100

    ratings page

--- json-load.js
+++ json-load.js
@@ -1,8 +1,9 @@
-var articleArray = [1,2,3,4,5];
+var articleArray = [4,5,1,3,2];
+var articleTitles = [];
 var randomIndex = Math.ceil(Math.random() * articleArray.length);
-var currentArticle = 4;
+var currentArticle = 0;
 
-var elements = {heading: 'h2', paragraph: 'p', image: 'img', list: 'ul', ordered: 'ol'}
+var elements = {heading: 'h2', paragraph: 'p', image: 'img', list: 'ul', ordered: 'ol'};
 
 $(document).ready(function(){
 	loadArticle(articleArray[currentArticle - 1], articleArray[currentArticle], articleArray[currentArticle + 1]);
@@ -10,8 +11,8 @@ $(document).ready(function(){
 
 $(document).on('click','#next', function(){
 	currentArticle++;
-	var myNode = document.querySelector('.article');
-	while (myNode.firstChild) {
+	var myNode = document.querySelector('.content');
+	while(myNode.firstChild){
 		myNode.removeChild(myNode.firstChild);
 	}
 	loadArticle(articleArray[currentArticle - 1], articleArray[currentArticle], articleArray[currentArticle + 1]);
@@ -19,28 +20,53 @@ $(document).on('click','#next', function(){
 
 $(document).on('click','#previous', function(){
 	currentArticle--;
-	var myNode = document.querySelector('.article');
-	while (myNode.firstChild) {
+	var myNode = document.querySelector('.content');
+	while(myNode.firstChild){
 		myNode.removeChild(myNode.firstChild);
 	}
 	loadArticle(articleArray[currentArticle - 1], articleArray[currentArticle], articleArray[currentArticle + 1]);
-}) 
+})
+
+$(document).on('click','#rate', function(){
+	var myNode = document.querySelector('.content');
+	while(myNode.firstChild){
+		myNode.removeChild(myNode.firstChild);
+	}
+	loadRater(articleArray);
+})
+
+$(document).on('click','.rateUp', function(){
+	var current = $(this).closest('li')
+	var previous = current.prev('li');
+	if(previous.length !== 0){
+		current.insertBefore(previous);
+	}
+});
+
+$(document).on('click','.rateDown', function(){
+	var current = $(this).closest('li')
+	var next = current.next('li');
+	if(next.length !== 0){
+		current.insertAfter(next);
+	}
+});
 
 function loadArticle(previous, current, next){
 	$.getJSON('api/article/get.php?id='+current, function(article){
 		article = JSON.parse(article.message);
-		var articleElement = document.querySelector('.article');
+		var contentElement = document.querySelector('.content');
 
 		var h1 = document.createElement('h1');
 		h1.textContent = article.title;
-		articleElement.appendChild(h1);
+		contentElement.appendChild(h1);
 		document.title = article.title;
+		articleTitles[current] = article.title;
 
 		var bodyDiv = document.createElement('div');
 		bodyDiv.className = 'article-body';
-		articleElement.appendChild(bodyDiv);
+		contentElement.appendChild(bodyDiv);
 
-		for (var element in article.body){
+		for(var element in article.body){
 			var elementType = elements[article.body[element].type];
 			var newElement = document.createElement(elementType);
 			if(elementType == 'img'){
@@ -54,7 +80,7 @@ function loadArticle(previous, current, next){
 					elementType = elements[article.body[element].model.type];
 					newElement = document.createElement(elementType);
 				}
-				for (var list in article.body[element].model.items){
+				for(var list in article.body[element].model.items){
 					var listItem = document.createElement('li');
 					listItem.textContent = article.body[element].model.items[list];
 					newElement.appendChild(listItem);
@@ -67,7 +93,7 @@ function loadArticle(previous, current, next){
 		}
 		var buttons = document.createElement('div');
 		buttons.className = 'nav-buttons';
-		articleElement.appendChild(buttons);
+		contentElement.appendChild(buttons);
 		if(previous){
 			var prevButton = document.createElement('button');
 			prevButton.textContent = 'Previous Article';
@@ -82,11 +108,45 @@ function loadArticle(previous, current, next){
 			nextButton.value = next;
 			buttons.appendChild(nextButton);
 		}
-		else {
+		else{
 			var rateButton = document.createElement('button');
 			rateButton.textContent = 'Rate Articles';
 			rateButton.id = 'rate';
 			buttons.appendChild(rateButton);
 		}
 	});
+}
+
+function loadRater(articleArray){
+	var contentElement = document.querySelector('.content');
+
+	var h1 = document.createElement('h1');
+	h1.textContent = 'Rate the articles';
+	contentElement.appendChild(h1);
+	document.title = h1.textContent;
+
+	var p = document.createElement('p');
+	p.textContent = 'Please rate the articles you have just read in order of preference, with the highest rated article at the top and the lowest rated article at the bottom';
+	contentElement.appendChild(p);
+
+	var ol = document.createElement('ol');
+	contentElement.appendChild(ol);
+	for(var article in articleArray){
+		var li = document.createElement('li');
+		li.textContent = articleTitles[articleArray[article]];
+		li.className = 'articleRateBox';
+		li.id = articleArray[article];
+		ol.appendChild(li);
+		var span = document.createElement('span');
+		span.className = 'pull-right';
+		li.appendChild(span);
+		var buttonUp = document.createElement('button');
+		buttonUp.textContent = 'Up';
+		buttonUp.className = 'rateUp';
+		span.appendChild(buttonUp);
+		var buttonDown = document.createElement('button');
+		buttonDown.textContent = 'Down';
+		buttonDown.className = 'rateDown';
+		span.appendChild(buttonDown);
+	}
 }
